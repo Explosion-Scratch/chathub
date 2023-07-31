7631f86161e2018cb2903f52ee56523a1714c613
Explosion Implosion <explosion.scratch@gmail.com>
2023-07-31 13:19:16 -0500

    Build

--- dist/service-worker-loader.js
+++ dist/service-worker-loader.js
@@ -1,3 +1,1 @@
-import 'http://localhost:5173/@vite/env';
-import 'http://localhost:5173/@crx/client-worker';
-import 'http://localhost:5173/src/background/index.ts';
+import './assets/index.ts-f8d90c41.js';
