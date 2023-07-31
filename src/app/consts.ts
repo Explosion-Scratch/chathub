hunght/buddybeep.com

Update open-source chatbots list

src/app/consts.ts
@@ -4,15 +4,14 @@ import bardLogo from '~/assets/bard-logo.svg'
 import bingLogo from '~/assets/bing-logo.svg'
 import chatglmLogo from '~/assets/chatglm-logo.svg'
 import chatgptLogo from '~/assets/chatgpt-logo.svg'
-import vicunaLogo from '~/assets/vicuna-logo.jpg'
-import xunfeiLogo from '~/assets/xunfei-logo.png'
-import koalaLogo from '~/assets/koala-logo.jpg'
-import dollyLogo from '~/assets/dolly-logo.png'
+import guanacoLogo from '~/assets/guanaco-logo.png'
 import llamaLogo from '~/assets/llama-logo.png'
-import stablelmLogo from '~/assets/stablelm-logo.png'
 import oasstLogo from '~/assets/oasst-logo.svg'
-import rwkvLogo from '~/assets/rwkv-logo.png'
 import piLogo from '~/assets/pi-logo.png'
+import rwkvLogo from '~/assets/rwkv-logo.png'
+import vicunaLogo from '~/assets/vicuna-logo.jpg'
+import wizardlmLogo from '~/assets/wizardlm-logo.png'
+import xunfeiLogo from '~/assets/xunfei-logo.png'
 import { BotId } from './bots'
 import i18n from './i18n'
 
@@ -57,19 +56,6 @@ export const CHATBOTS: Record<BotId, { name: string; avatar: any }> = {
     name: i18n.t('iFlytek Spark'),
     avatar: xunfeiLogo,
   },
-  koala: {
-    name: 'Koala',
-    avatar: koalaLogo,
-  },
-  dolly: {
-    name: 'Dolly',
-    avatar: dollyLogo,
-  },
-
-  stablelm: {
-    name: 'StableLM',
-    avatar: stablelmLogo,
-  },
   oasst: {
     name: 'OpenAssistant',
     avatar: oasstLogo,
@@ -77,6 +63,14 @@ export const CHATBOTS: Record<BotId, { name: string; avatar: any }> = {
   rwkv: {
     name: 'ChatRWKV',
     avatar: rwkvLogo,
+  },
+  guanaco: {
+    name: 'Guanaco',
+    avatar: guanacoLogo,
+  },
+  wizardlm: {
+    name: 'WizardLM',
+    avatar: wizardlmLogo,
   },
 }
 

Explosion-Scratch/chathub

src/app/consts.ts
@@ -13,6 +13,10 @@ import vicunaLogo from '~/assets/vicuna-logo.jpg'
 import wizardlmLogo from '~/assets/wizardlm-logo.png'
 import xunfeiLogo from '~/assets/xunfei-logo.png'
 import piLogo from '~/assets/pi-logo.png'
+import rwkvLogo from '~/assets/rwkv-logo.png'
+import vicunaLogo from '~/assets/vicuna-logo.jpg'
+import wizardlmLogo from '~/assets/wizardlm-logo.png'
+import xunfeiLogo from '~/assets/xunfei-logo.png'
 import { BotId } from './bots'
 import i18n from './i18n'
 
@@ -64,6 +68,10 @@ export const CHATBOTS: Record<BotId, { name: string; avatar: any }> = {
   stablelm: {
     name: 'StableLM',
     avatar: stablelmLogo,
+  },
+  xunfei: {
+    name: i18n.t('iFlytek Spark'),
+    avatar: xunfeiLogo,
   },
   oasst: {
     name: 'OpenAssistant',